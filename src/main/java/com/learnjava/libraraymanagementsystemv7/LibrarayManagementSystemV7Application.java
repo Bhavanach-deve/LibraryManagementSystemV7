2c0e651fc4c8a8268227770d1779ae32b937e048
bhavanach-deve <bhavanachilkeshwaram@gmail.com>
2026-08-18 23:20:27 +0530

    Initial Commit- version7 LibraryManagementSystem loading!

---
+++ src/main/java/com/learnjava/libraraymanagementsystemv7/LibrarayManagementSystemV7Application.java
@@ -0,0 +1,13 @@
+package com.learnjava.libraraymanagementsystemv7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrarayManagementSystemV7Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibrarayManagementSystemV7Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/learnjava/libraraymanagementsystemv7/LibrarayManagementSystemV7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnjava.libraraymanagementsystemv7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrarayManagementSystemV7ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
